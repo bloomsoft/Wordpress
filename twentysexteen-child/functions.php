af0aa1ff461d7ac14b77aaebca8e42e93b351593
post-primary-category <bloomsoft@gmail.com>
2017-01-12 14:13:58 +0500

    twentysixteen-child-theme-demo
    This is a child theme demo shows how you can create a child theme with
    modifications in widgets area and footer in the default twenty sixteen
    theme comes with wordpress installation.

---
+++ twentysexteen-child/functions.php
@@ -0,0 +1,108 @@
+<?php 
+//Import parent styles the right way and add other stylesheets if necessary.
+function twenty_sixteen_child_styles() {
+  wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+}
+add_action( 'wp_enqueue_scripts', 'twenty_sixteen_child_styles' );
+
+function tutsplus_widgets_init() {
+ 
+    // First footer widget area, located in the footer. Empty by default.
+    register_sidebar( array(
+        'name' => __( 'First Footer Widget Area', 'tutsplus' ),
+        'id' => 'first-footer-widget-area',
+        'description' => __( 'The first footer widget area', 'tutsplus' ),
+        'before_widget' => '<div id="%1$s" class="widget-container %2$s">',
+        'after_widget' => '</div>',
+        'before_title' => '<h3 class="widget-title">',
+        'after_title' => '</h3>',
+    ) );
+ 
+    // Second Footer Widget Area, located in the footer. Empty by default.
+    register_sidebar( array(
+        'name' => __( 'Second Footer Widget Area', 'tutsplus' ),
+        'id' => 'second-footer-widget-area',
+        'description' => __( 'The second footer widget area', 'tutsplus' ),
+        'before_widget' => '<div id="%1$s" class="widget-container %2$s">',
+        'after_widget' => '</div>',
+        'before_title' => '<h3 class="widget-title">',
+        'after_title' => '</h3>',
+    ) );
+ 
+    // Third Footer Widget Area, located in the footer. Empty by default.
+    register_sidebar( array(
+        'name' => __( 'Third Footer Widget Area', 'tutsplus' ),
+        'id' => 'third-footer-widget-area',
+        'description' => __( 'The third footer widget area', 'tutsplus' ),
+        'before_widget' => '<div id="%1$s" class="widget-container %2$s">',
+        'after_widget' => '</div>',
+        'before_title' => '<h3 class="widget-title">',
+        'after_title' => '</h3>',
+    ) );
+ 
+    // Fourth Footer Widget Area, located in the footer. Empty by default.
+    register_sidebar( array(
+        'name' => __( 'Fourth Footer Widget Area', 'tutsplus' ),
+        'id' => 'fourth-footer-widget-area',
+        'description' => __( 'The fourth footer widget area', 'tutsplus' ),
+        'before_widget' => '<div id="%1$s" class="widget-container %2$s">',
+        'after_widget' => '</div>',
+        'before_title' => '<h3 class="widget-title">',
+        'after_title' => '</h3>',
+    ) );
+         
+}
+ 
+// Register sidebars by running tutsplus_widgets_init() on the widgets_init hook.
+add_action( 'widgets_init', 'tutsplus_widgets_init' );
+
+function wpb_custom_new_menu() {
+  register_nav_menus(
+    array(
+      'my-custom-menu' => __( 'My Custom Menu' ),
+      'extra-menu' => __( 'Extra Menu' )
+    )
+  );
+}
+add_action( 'init', 'wpb_custom_new_menu' );
+
+/**
+ * Register meta box(es).
+ */
+function wpdocs_register_meta_boxes() {
+    add_meta_box( 'meta-box-id', __( 'Visitors Analysis', 'textdomain' ), 'wpdocs_my_display_callback', 'post' );
+}
+add_action( 'add_meta_boxes', 'wpdocs_register_meta_boxes' );
+ 
+/**
+ * Meta box display callback.
+ *
+ * @param WP_Post $post Current post object.
+ */
+function wpdocs_my_display_callback( $post ) {
+    // Display code/markup goes here. Don't forget to include nonces!
+	 // Add an nonce field so we can check for it later.
+        wp_nonce_field( 'meta-box-id', 'meta_box_id_nonce' );
+ 
+        // Use get_post_meta to retrieve an existing value from the database.
+        $value = get_post_meta( $post->ID, '_my_meta_value_key', true );
+ 
+        // Display the form, using the current value.
+        ?>
+        <label for="myplugin_new_field">
+            <?php _e( 'Description for this field', 'textdomain' ); ?>
+        </label>
+        <input type="text" id="myplugin_new_field" name="myplugin_new_field" value="<?php echo esc_attr( $value ); ?>" size="25" />
+        <?php
+}
+ 
+/**
+ * Save meta box content.
+ *
+ * @param int $post_id Post ID
+ */
+function wpdocs_save_meta_box( $post_id ) {
+    // Save logic goes here. Don't forget to include nonce checks!
+}
+add_action( 'save_post', 'wpdocs_save_meta_box' );
+
